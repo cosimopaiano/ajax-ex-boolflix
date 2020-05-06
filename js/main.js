3f7553bdb1ded6ab76ecb117ca1df9f986c30549
cosimopaiano <cosimo.paiano@libero.it>
2020-05-06 21:15:11 +0200

    Milestone 3-4 to complete

--- js/main.js
+++ js/main.js
@@ -21,6 +21,25 @@
 * Allarghiamo poi la ricerca anche alle serie tv.
 */
 
+/**
+* Milestone 3:
+*In questa milestone come prima cosa  faremo un refactor delle chiamate ajax creando un’unica funzione
+*alla quale passeremo la url, la apy key, la query, il type, ecc… 
+*In questo modo potremo chiamare la ricerca sia con il keypress su enter che con il click.
+*Poi, aggiungiamo la copertina del film o della serie al nostro elenco.
+*Ci viene passata dall’API solo la parte finale dell’URL,
+*questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
+*/
+
+/**
+*Milestone 4:
+*Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp, creando un layout completo simil-Netflix:
+*Un header che contiene logo e search bar
+*Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma di “card” 
+*in cui lo sfondo è rappresentato dall’immagine di copertina (consiglio la poster_path con w342)
+*Andando con il mouse sopra una card (on hover), appaiono le informazioni aggiuntive già prese nei punti precedenti più la overview
+*/
+
 //Chiave API :f8d027bbf340b6b3bbe32c9525262297
 
 $(document).ready(function () {
@@ -44,11 +63,34 @@ $(document).ready(function () {
         apiSearch(searchTitle, template, 'movie');
     });
 
+    // Ricerca film con tasto enter
+    inputSearch.keyup(function(e) {
+        
+        if(e.which === 13 || e.keyCode === 13) {
+            var searchTitle = inputSearch.val().trim();
+            $('.movie-list').html('');
+            apiSearch(searchTitle, template, 'tv');
+            apiSearch(searchTitle, template, 'movie');
+        }
+    });   
+
+    
+    // mostra dettagli film in hover su immagine
+    $('body').on('mouseenter', '.movie', function() {
+        $(this).children('.movie-list-details').fadeIn();
+        $(this).children('.movie-poster').addClass('active');
+    });
+
+    $('body').on('mouseleave', '.movie', function() {
+        $(this).children('.movie-list-details').fadeOut();
+        $(this).children('.movie-poster').removeClass('active');
+    });
+
 }); // end doc ready
 
 //functions
 
-// Ajax movie
+// Ajax movie e tv
 function apiSearch(search, template, type) {
     $.ajax({
         url: "https://api.themoviedb.org/3/search/" + type,
@@ -73,6 +115,17 @@ function apiSearch(search, template, type) {
 
 // Funzione per visualizzare il template
 function visualTemplate(item, template, type) {
+
+    //aggiunta locandina
+    var posterPath = {
+        url: 'https://image.tmdb.org/t/p/',
+        posterSize: 'w342',
+        imageUrl: item.poster_path
+    };
+
+    var poster = finalPoster(posterPath);  
+    //  var poster = posterPath.url + posterPath.posterSize + posterPath.imageUrl;
+
     var context;
     if(type === 'movie'){
 
@@ -81,31 +134,46 @@ function visualTemplate(item, template, type) {
             originalTitle:'Titolo originale: ' + item.original_title,
             language: 'Lingua originale: ' +  languageFlag(item.original_language),
             averageVote: 'Voto: ' +  voteStars(item.vote_average),
-            type: 'Categoria: ' + type 
+            type: 'Categoria: ' + type,
+            overview: 'Overview: ' + item.overview.substr(0, 500) + '...',
+            poster: poster
         }
     } else {
         context = {
             title: 'Titolo: ' + item.name,
-            originalTitle:'Titolo originale: ' + item.original_title,
+            originalTitle:'Titolo originale: ' + item.original_name,
             language: 'Lingua originale: ' +  languageFlag(item.original_language),
             averageVote: 'Voto: ' +  voteStars(item.vote_average),
-            type: 'Categoria: ' + type 
+            type: 'Categoria: ' + type,
+            overview: 'Overview: ' + item.overview.substr(0, 500) + '...',
+            poster: poster
         }
        
-
     }
 
     var outputMovie = template(context);
     $('.movie-list').append(outputMovie);
 };
 
+//In caso di mancata locandina sostituisce con un 'no image'
+function finalPoster(posterPath) {
+    if(posterPath.imageUrl !== null){
+        return  posterPath.url + posterPath.posterSize + posterPath.imageUrl;
+    } else {
+        return 'img/no-poster.png'
+    }
+
+}
+
+
+
 // trasforma votazione da num. decimale ad intero e lo sostituisce con le stars
 function voteStars(number){
     
     var value = Math.ceil(number/2);
 
-    var stars = "";
-    for(var i = 1; i<= 5; i++){
+    var stars = " ";
+    for(var i = 1; i <= 5; i++){
         if(i <= value){
             stars += '<i class="fas fa-star"></i>';
         }else{
